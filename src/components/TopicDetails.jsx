2b103909bac863da486a15bd55b1700bbd9fdced
Nishat <nishatbd17@gmail.com>
2023-06-11 01:03:46 +0600

    topic quiz details done

--- src/components/TopicDetails.jsx
+++ src/components/TopicDetails.jsx
@@ -12,7 +12,7 @@ const TopicDetails = () => {
         <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
             <h2 className='text-3xl font-semibold text-center'>Quiz of {name}</h2>
             <img src={logo} alt="" />
-
+            <p>{questions}</p>
         </div>
     );
 };
