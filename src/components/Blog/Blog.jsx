d3c2126dff5531dfba3e8a75546694dd89c7456c
Nishat <nishatbd17@gmail.com>
2023-06-10 13:19:19 +0600

    README.md updated

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,69 +2,23 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <section className="py-6 sm:py-12 dark:bg-gray-800 dark:text-gray-100">
-            <div className="container p-6 mx-auto space-y-8">
-                <div className="space-y-2 text-center">
-                    <h2 className="text-3xl font-bold">Partem reprimique an pro</h2>
-                    <p className="font-serif text-sm dark:text-gray-400">Qualisque erroribus usu at, duo te agam soluta mucius.</p>
-                </div>
-                <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
-                    <article className="flex flex-col dark:bg-gray-900">
-                        <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-                            <img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://source.unsplash.com/200x200/?fashion?1" />
-                        </a>
-                        <div className="flex flex-col flex-1 p-6">
-                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-                            <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-400">Convenire</a>
-                            <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">Te nulla oportere reprimique his dolorum</h3>
-                            <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
-                                <span>June 1, 2020</span>
-                                <span>2.1K views</span>
-                            </div>
-                        </div>
-                    </article>
-                    <article className="flex flex-col dark:bg-gray-900">
-                        <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-                            <img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://source.unsplash.com/200x200/?fashion?2" />
-                        </a>
-                        <div className="flex flex-col flex-1 p-6">
-                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-                            <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-400">Convenire</a>
-                            <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">Te nulla oportere reprimique his dolorum</h3>
-                            <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
-                                <span>June 2, 2020</span>
-                                <span>2.2K views</span>
-                            </div>
-                        </div>
-                    </article>
-                    <article className="flex flex-col dark:bg-gray-900">
-                        <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-                            <img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://source.unsplash.com/200x200/?fashion?3" />
-                        </a>
-                        <div className="flex flex-col flex-1 p-6">
-                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-                            <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-400">Convenire</a>
-                            <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">Te nulla oportere reprimique his dolorum</h3>
-                            <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
-                                <span>June 3, 2020</span>
-                                <span>2.3K views</span>
-                            </div>
-                        </div>
-                    </article>
-                    <article className="flex flex-col dark:bg-gray-900">
-                        <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-                            <img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://source.unsplash.com/200x200/?fashion?4" />
-                        </a>
-                        <div className="flex flex-col flex-1 p-6">
-                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-                            <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-400">Convenire</a>
-                            <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">Te nulla oportere reprimique his dolorum</h3>
-                            <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
-                                <span>June 4, 2020</span>
-                                <span>2.4K views</span>
-                            </div>
-                        </div>
-                    </article>
+        <section className="h-screen text-gray-100 bg-gray-800">
+            <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+                <h2 className="text-2xl font-semibold sm:text-4xl">Some Questions Answer</h2>
+                <p className="mt-4 mb-8 text-gray-400">Some basic question answers are given below.</p>
+                <div className="space-y-4">
+                    <details className="w-full border rounded-lg" open="">
+                        <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400"> What is the purpose of React Router?</summary>
+                        <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-400">Lectus iaculis orci metus vitae ligula dictum per. Nisl per nullam taciti at adipiscing est. </p>
+                    </details>
+                    <details className="w-full border rounded-lg" open="">
+                        <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400"> How does Context API work?</summary>
+                        <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-400">Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna porttitor egestas tincidunt neque vehicula potenti. </p>
+                    </details>
+                    <details className="w-full border rounded-lg" open="">
+                        <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">Explain useRef (5 to 10 lines).</summary>
+                        <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-400">Justo libero tellus integer tincidunt justo semper consequat venenatis aliquet imperdiet. Ultricies urna proin fusce nulla pretium sodales vel magna et massa euismod vulputate sed. </p>
+                    </details>
                 </div>
             </div>
         </section>
